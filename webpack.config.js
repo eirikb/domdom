8b13d2390db50b5226d58096f3414d384d5b10a7
Eirik Brandtzæg <eirikb@eirikb.no>
2019-05-06 23:57:46 +0200

    Webpack: Moved module

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,6 @@ const path = require('path');
 module.exports = {
   target: 'web',
   mode: 'production',
-  entry: [
-    './domdom/index.js'
-  ],
   output: {
     path: path.join(__dirname, 'dist'),
     library: 'domdom',
