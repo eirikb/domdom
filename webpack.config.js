dcb4fad92c799b60eadbba183f4f31292aa86138
Eirik Brandtzæg <eirikb@eirikb.no>
2019-05-12 19:30:39 +0200

    Export library default

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     library: 'domdom',
     filename: 'domdom.js',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
+    libraryExport: 'default'
   }
 };
