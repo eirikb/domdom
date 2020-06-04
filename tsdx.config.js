46989ad43d361f9d4f0f78c736ecc9199b8c76cd
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-04 13:44:05 +0530

    feat: add tsdx config

---
+++ tsdx.config.js
@@ -0,0 +1,11 @@
+const extensions = './extensions.js';
+// Not transpiled with TypeScript or Babel, so use plain Es6/Node.js!
+module.exports = {
+  // This function will run for each entry/format/env combination
+  rollup(config, options) {
+     config.plugins.push(extensions({
+       extensions: [ '.jsx', '.tsx', '.js', '.ts' ]
+     }))
+    return config; // always return a config.
+  },
+};
