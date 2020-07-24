78aa5d63aa7e0173a5fa5a549883d809c6e21dbd
Eirik Brandtzæg <eirikb@eirikb.no>
2020-07-24 22:51:50 +0200

    Convert Hodor to class

--- src/hodor.ts
+++ src/hodor.ts
@@ -1,4 +1,5 @@
-import Data, {
+import {
+  Data,
   SorterOn,
   Sorter,
   FilterOn,
@@ -8,168 +9,174 @@ import Data, {
   Callback,
   LooseObject,
 } from '@eirikb/data';
-import { Domode, Hodor } from './types';
+import { Domode } from './types';
 
-export default (data: Data, path: string, listener?: Callback): Hodor => {
-  const listenerSet = !!listener;
-  if (listener === undefined) {
-    listener = (_: any) => _;
+export class Hodor {
+  data: Data;
+  listeners: { flagsAndPath: string; cb: Function; ref: string }[] = [];
+  isMounted = false;
+  path: string;
+  element?: Domode;
+  isHodor = true;
+  _stower?: Stower;
+  _or?: Function;
+  index?: number;
+  pathifier?: Pathifier;
+  listening?: boolean;
+  _filter?: Filter;
+  _filterOn?: { path: string; filterOn: FilterOn };
+  _sort?: Sorter;
+  _sortOn?: { path: string; sorterOn: SorterOn };
+  _map?: Callback;
+  listenerSet = false;
+  paths: string[] = [];
+  listener?: Callback;
+
+  constructor(data: Data, path: string, listener?: Callback) {
+    this.data = data;
+    this.listenerSet = !!listener;
+    if (listener === undefined) {
+      listener = (_: any) => _;
+    }
+    this.listener = listener;
+    if (typeof listener !== 'function') {
+      throw new Error('Listener must be a function');
+    }
+
+    const hasFlags = path.match(/ /);
+    if (hasFlags) {
+      this.on(path, listener);
+    }
+    this.path = path;
   }
-  if (typeof listener !== 'function') {
-    throw new Error('Listener must be a function');
+
+  on(flagsAndPath: string, cb: Callback) {
+    const ref = this.data.on(flagsAndPath, cb);
+    this.listeners.push({ flagsAndPath, cb, ref });
   }
 
-  let stower: Stower,
-    _or: Function,
-    index: number,
-    pathifier: Pathifier,
-    listening: boolean;
-  let _filter: Filter,
-    _filterOn: { path: string; filterOn: FilterOn },
-    _sort: Sorter,
-    _sortOn: { path: string; sorterOn: SorterOn },
-    _map: Callback;
-
-  const listeners: { flagsAndPath: string; cb: Function; ref: string }[] = [];
-
-  function on(flagsAndPath: string, cb: Callback) {
-    const ref = data.on(flagsAndPath, cb);
-    listeners.push({ flagsAndPath, cb, ref });
+  or(or: Function) {
+    this._or = or;
+    return this;
   }
+  filter(filter: Filter) {
+    this._filter = filter;
+    return this;
+  }
+  filterOn(path: string, filterOn: FilterOn) {
+    this._filterOn = { path, filterOn };
+    return this;
+  }
+  sort(sort: Sorter) {
+    this._sort = sort;
+    return this;
+  }
+  sortOn(path: string, sorterOn: SorterOn) {
+    this._sortOn = { path, sorterOn };
+    return this;
+  }
+  map(map: Callback) {
+    if (this.listenerSet) {
+      throw new Error(`Sorry, can't combine listener and map`);
+    }
+    this._map = map;
+    return this;
+  }
+  stower(i: number, s: Stower) {
+    this.index = i;
+    this._stower = s;
+    if (this._or) {
+      this._stower.or(i, this._or);
+    }
+    return this;
+  }
+  mounted() {
+    if (this.isMounted) {
+      return;
+    }
+    this.isMounted = true;
+    if (typeof this.listen === 'function') {
+      this.listen(this.path);
+    }
+  }
+  destroy() {
+    this.isMounted = false;
+    this.off();
+  }
+  off() {
+    if (this.pathifier) this.pathifier.off();
+    for (let listener of this.listeners.filter(l => l.ref)) {
+      this.data.off(listener.ref);
+      delete listener.ref;
+    }
+    this.listening = false;
+  }
+  listen(path) {
+    if (this.listening) {
+      return;
+    }
+    this.listening = true;
+    if (!this._stower) {
+      return;
+    }
 
-  let isMounted = false;
-  const hodor: Hodor = {
-    path,
-    element: null,
-    isHodor: true,
-    or(or: Function) {
-      _or = or;
-      return hodor;
-    },
-    filter(filter: Filter) {
-      _filter = filter;
-      return hodor;
-    },
-    filterOn(path: string, filterOn: FilterOn) {
-      _filterOn = { path, filterOn };
-      return hodor;
-    },
-    sort(sort: Sorter) {
-      _sort = sort;
-      return hodor;
-    },
-    sortOn(path: string, sorterOn: SorterOn) {
-      _sortOn = { path, sorterOn };
-      return hodor;
-    },
-    map(map: Callback) {
-      if (listenerSet) {
-        throw new Error(`Sorry, can't combine listener and map`);
-      }
-      _map = map;
-      return hodor;
-    },
-    stower(i: number, s: Stower) {
-      index = i;
-      stower = s;
-      if (_or) {
-        stower.or(index, _or);
-      }
-      return hodor;
-    },
-    mounted() {
-      if (isMounted) {
-        return;
-      }
-      isMounted = true;
-      if (typeof hodor.listen === 'function') {
-        hodor.listen(path);
-      }
-    },
-    destroy() {
-      isMounted = false;
-      hodor.off();
-    },
-    off() {
-      if (pathifier) pathifier.off();
-      for (let listener of listeners.filter(l => l.ref)) {
-        data.off(listener.ref);
-        delete listener.ref;
-      }
-      listening = false;
-    },
-    paths: [],
-    listen: path => {
-      if (listening) {
-        return;
-      }
-      listening = true;
-      if (!stower) {
-        return;
-      }
-
-      if (hodor.element) {
-        let parentNode = hodor.element;
-        while (parentNode && parentNode.parentNode) {
-          if (parentNode.path) {
-            path = path.replace(/^>/, parentNode.path);
-            break;
-          }
-          parentNode = parentNode.parentNode as Domode;
+    if (this.element) {
+      let parentNode = this.element;
+      // TODO: Remove indexing
+      while (parentNode && parentNode['parentNode']) {
+        if (parentNode['path']) {
+          path = path.replace(/^>/, parentNode['path']);
+          break;
         }
+        parentNode = parentNode['parentNode'] as Domode;
       }
+    }
 
-      if (!_map) {
-        on(`!+* ${path}`, (val: any, props: LooseObject) => {
-          const path = props.path;
-          const subIndex = hodor.paths.indexOf(path);
-          if (subIndex >= 0) {
-            hodor.paths.splice(subIndex, 1);
-            stower.remove(null, index, subIndex);
-          }
-          const res = listener!(val, props) as any;
-          if (res instanceof Element) {
-            (res as any).path = path;
-          }
-          stower.add(res, index, hodor.paths.length, path);
-          hodor.paths.push(path);
-        });
-        on(`- ${path}`, (_: any, props: LooseObject) => {
-          const path = props.path;
-          const subIndex = hodor.paths.indexOf(path);
-          hodor.paths.splice(subIndex, 1);
-          stower.remove(null, index, subIndex);
-        });
-        return;
-      }
-      pathifier = data.on(path);
-      if (_map) pathifier.map(_map);
-      if (_filter) pathifier.filter(_filter);
-      if (_filterOn) pathifier.filterOn(_filterOn.path, _filterOn.filterOn);
-      if (_sort) pathifier.sort(_sort);
-      if (_sortOn) pathifier.sortOn(_sortOn.path, _sortOn.sorterOn);
-      pathifier.toArray({
-        or(_: number, __: any): void {},
-
-        add(value: any, subIndex: number, _?: number, path?: string) {
-          if (typeof value === 'object') {
-            value.path = [(pathifier as any).from, path].join('.');
-          }
-
-          stower.add(value, index, subIndex, path);
-        },
-        remove(value: any, subIndex: number, _: number, path: string) {
-          stower.remove(value, index, subIndex, path);
-        },
+    if (!this._map) {
+      this.on(`!+* ${path}`, (val: any, props: LooseObject) => {
+        const path = props.path;
+        const subIndex = this.paths.indexOf(path);
+        if (subIndex >= 0) {
+          this.paths.splice(subIndex, 1);
+          this._stower?.remove(null, this.index!, subIndex);
+        }
+        const res = this.listener!(val, props) as any;
+        if (res instanceof Element) {
+          (res as any).path = path;
+        }
+        this._stower?.add(res, this.index!, this.paths.length, path);
+        this.paths.push(path);
       });
-    },
-  };
+      this.on(`- ${path}`, (_: any, props: LooseObject) => {
+        const path = props.path;
+        const subIndex = this.paths.indexOf(path);
+        this.paths.splice(subIndex, 1);
+        this._stower?.remove(null, this.index!, subIndex);
+      });
+      return;
+    }
+    this.pathifier = this.data.on(path);
+    if (this._map) this.pathifier.map(this._map);
+    if (this._filter) this.pathifier.filter(this._filter);
+    if (this._filterOn)
+      this.pathifier.filterOn(this._filterOn.path, this._filterOn.filterOn);
+    if (this._sort) this.pathifier.sort(this._sort);
+    if (this._sortOn)
+      this.pathifier.sortOn(this._sortOn.path, this._sortOn.sorterOn);
+    const self = this;
+    this.pathifier.toArray({
+      or(_: number, __: any): void {},
 
-  const hasFlags = path.match(/ /);
-  if (hasFlags) {
-    on(path, listener);
-    return hodor;
+      add(value: any, subIndex: number, _?: number, path?: string) {
+        if (typeof value === 'object') {
+          value.path = [(self.pathifier as any).from, path].join('.');
+        }
+
+        self._stower?.add(value, self.index!, subIndex, path);
+      },
+      remove(value: any, subIndex: number, _: number, path: string) {
+        self._stower?.remove(value, self.index!, subIndex, path);
+      },
+    });
   }
-
-  return hodor;
-};
+}
