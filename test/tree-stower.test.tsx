94b51a36356cfe1de017111a577a9daad7e086ce
Eirik Brandtzæg <eirikb@eirikb.no>
2021-06-08 20:48:12 +0200

    Fix tests

--- test/tree-stower.test.tsx
+++ test/tree-stower.test.tsx
@@ -23,12 +23,12 @@ async function html() {
 }
 
 test('array initial', async t => {
-  const { React } = domdom();
+  const { React } = domdom({});
   t.is((<div>{[1, [2, [3, 4], 5, 6]]}</div>).outerHTML, '<div>123456</div>');
 });
 
 test('array replace', async t => {
-  const { React, init, don, set } = domdom();
+  const { React, init, don, set } = domdom({});
   init(createElement(), <div>{don('x.*')}</div>);
   set('x', ['a']);
   t.is(await html(), '<div>a</div>');
@@ -37,7 +37,7 @@ test('array replace', async t => {
 });
 
 test('array replace 2', async t => {
-  const { React, init, don, set } = domdom();
+  const { React, init, don, set } = domdom({});
   init(createElement(), <div>{don('x.*')}</div>);
   set('x', [[['a']]]);
   t.is(await html(), '<div>a</div>');
@@ -47,7 +47,7 @@ test('array replace 2', async t => {
 });
 
 test('on', async t => {
-  const { React, init, don, set } = domdom();
+  const { React, init, don, set } = domdom({});
   init(
     createElement(),
     <div>
